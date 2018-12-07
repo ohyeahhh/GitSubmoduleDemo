045f7daa70b74ab01abbfc681de9160ba3b23699
ohyeah <ohyeahmisoh@gmail.com>
2018-12-07 14:47:35 +0800

    嵌入子项目，并变更了大项目和小项目

--- SubmoduleDemo/SubmoduleDemo/FirstViewController.m
+++ SubmoduleDemo/SubmoduleDemo/FirstViewController.m
@@ -6,18 +6,23 @@
 //  Copyright © 2018 mac. All rights reserved.
 //
 
+#import "FirstViewController.h"
 #import "ViewController.h"
-
-@interface ViewController ()
+@interface FirstViewController ()
 
 @end
 
-@implementation ViewController
+@implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)pushToOYDatePicker:(id)sender {
+    [self.navigationController pushViewController:  [[ViewController alloc]init] animated:YES];
+  
+    
+}
 
 @end
